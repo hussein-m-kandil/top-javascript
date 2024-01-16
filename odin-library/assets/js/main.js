b054eb13e2f3352a6adebb1b77b80278fc7c0d92
Hussein Kandil <101815486+hussein-m-kandil@users.noreply.github.com>
2024-01-16 13:10:50 +0200

    Odin Library: Add 'Book' constructor and test it

--- odin-library/assets/js/main.js
+++ odin-library/assets/js/main.js
@@ -1,9 +1,50 @@
 const myLibrary = [];
 
-function Book() {
-  // the constructor...
+// the constructor
+function Book(id, title, author, numOfPages, read) {
+  if (!(this instanceof Book)) {
+    throw Error(
+      "Invalid invocation \n" +
+        "\nOnly constructor invocation allowed; " +
+        "invoke 'Book' using 'new'.\n"
+    );
+  }
+
+  this.id = id;
+  this.title = title;
+  this.author = author;
+  this.numOfPages = numOfPages;
+  this.read = read;
 }
 
-function addBookToLibrary() {
-  // do stuff here
-}
+Book.prototype.isRead = function (read) {
+  this.read = read;
+};
+
+// Add some dummy books to the myLibrary
+myLibrary.push(
+  new Book(
+    String(myLibrary.length + 1),
+    "Murder On The Orient Express",
+    "Agatha Christie",
+    "256",
+    false
+  )
+);
+myLibrary.push(
+  new Book(
+    String(myLibrary.length + 1),
+    "Death on the Nile",
+    "Agatha Christie",
+    "288",
+    false
+  )
+);
+console.table(myLibrary);
+myLibrary.forEach((book) => {
+  if (book.id === "1") book.isRead(true);
+});
+console.table(myLibrary);
+
+// Add new book to the DOM & myLibrary
+function addBookToLibrary() {}
