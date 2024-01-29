1abcfcbaf09a6dd64a2662d2ca0f2c00d088432a
Hussein Kandil <101815486+hussein-m-kandil@users.noreply.github.com>
2024-01-29 13:01:05 +0200

    Odin Tic Tac Toe: Add game events & test win event

--- odin-tic-tac-toe/assets/js/main.js
+++ odin-tic-tac-toe/assets/js/main.js
@@ -1,31 +1,65 @@
-function createPlayer(name, type) {
-  let score = 0;
+const gameEvents = (function () {
+  const events = {};
 
-  function incrementScore() {
-    return ++score;
+  function add(eventName, callbackfn) {
+    if (typeof eventName === "string" && typeof callbackfn === "function") {
+      events[eventName] = events[eventName] ?? [];
+      events[eventName].push(callbackfn);
+    }
   }
 
-  function render() {
-    console.log(type + " Player, " + name + ", Score: " + score);
+  function remove(eventName, callbackfn) {
+    if (typeof eventName === "string") {
+      const callbacks = events[eventName];
+      if (Array.isArray(callbacks)) {
+        const i = callbacks.indexOf(callbackfn);
+        if (i > -1) {
+          callbacks.splice(i, 1);
+        }
+      }
+    }
   }
 
-  return { incrementScore, render };
-}
+  function emit(eventName) {
+    if (typeof eventName === "string") {
+      const callbacks = events[eventName];
+      if (Array.isArray(callbacks)) {
+        callbacks.forEach((callbackfn) => callbackfn());
+      }
+    }
+  }
+
+  return { add, remove, emit };
+})();
 
 const gameBoard = (function () {
   const board = Array(9).fill("", 0);
 
-  function isWinner() {
-    if (
-      (board[0] === board[1] && board[1] === board[2]) || // 1st row
-      (board[0] === board[4] && board[4] === board[8]) || // Diagonal
-      (board[3] === board[4] && board[4] === board[5]) || // 2nd row
-      (board[6] === board[7] && board[7] === board[8]) || // 3rd row
-      (board[6] === board[4] && board[4] === board[2]) // Diagonal
-    ) {
-      return true;
+  function validateIndex(placeIndex) {
+    const i = Number(placeIndex);
+    if (Number.isInteger(i) && i >= 0 && i < 9) {
+      return i;
     }
-    return false;
+    return -1;
+  }
+
+  function isValidPlace(placeIndex) {
+    return !board[placeIndex];
+  }
+
+  function checkForWin() {
+    if (
+      (board[0] && board[0] === board[1] && board[1] === board[2]) || // 1st row
+      (board[3] && board[3] === board[4] && board[4] === board[5]) || // 2nd row
+      (board[6] && board[6] === board[7] && board[7] === board[8]) || // 3rd row
+      (board[0] && board[0] === board[3] && board[3] === board[6]) || // 1st column
+      (board[1] && board[1] === board[4] && board[4] === board[7]) || // 2nd column
+      (board[2] && board[2] === board[5] && board[5] === board[8]) || // 3rd column
+      (board[0] && board[0] === board[4] && board[4] === board[8]) || // Diagonal
+      (board[6] && board[6] === board[4] && board[4] === board[2]) // Diagonal
+    ) {
+      gameEvents.emit("win");
+    }
   }
 
   function clear() {
@@ -40,27 +74,98 @@ const gameBoard = (function () {
     ]);
   }
 
-  return { render, clear };
-})();
-
-const display = (function () {
-  function render(gameBoard, x, o) {
-    gameBoard.render();
-    x.render();
-    o.render();
+  function mark(placeIndex, marker) {
+    const i = validateIndex(placeIndex);
+    if (i > -1) {
+      if (isValidPlace(i)) {
+        board[i] = marker;
+        render();
+        checkForWin();
+      }
+    }
   }
 
-  return { render };
+  function isEmptyPlace() {
+    for (let i = 0; i < board.length; i++) {
+      if (!board[i]) {
+        return true;
+      }
+    }
+  }
+
+  return { clear, render, mark, isEmptyPlace };
 })();
 
+function createPlayer(name, type) {
+  let score = 0;
+
+  function getName() {
+    return name;
+  }
+
+  function getType() {
+    return type;
+  }
+
+  function getScore() {
+    return score;
+  }
+
+  function incrementScore() {
+    return ++score;
+  }
+
+  function render() {
+    console.log(type + " Player, " + name + ", Score: " + score);
+  }
+
+  return { getName, getType, getScore, incrementScore, render };
+}
+
 // Create players
-let xPlayerName, oPlayerName;
+let xPlayerName = "Foo",
+  oPlayerName = "Baz";
 // do {
 // xPlayerName = window.prompt("X Player Name: ");
 // oPlayerName = window.prompt("O Player Name: ");
 // } while (!xPlayerName || !oPlayerName);
 const xPlayer = createPlayer(xPlayerName, "X");
 const oPlayer = createPlayer(oPlayerName, "O");
+const players = [xPlayer, oPlayer];
 
 // Play a game
-display.render(gameBoard, xPlayer, oPlayer);
+gameBoard.render(), xPlayer.render(), oPlayer.render();
+
+let win = false;
+function onWin() {
+  win = true;
+}
+gameEvents.add("win", onWin);
+const usedPlaces = [];
+let currentPlayer,
+  i = 0;
+while (gameBoard.isEmptyPlace() && !win) {
+  let randomPlaceIndex; // Choose a random, not used place
+  do {
+    randomPlaceIndex = Math.floor(Math.random() * 9);
+  } while (usedPlaces.includes(randomPlaceIndex));
+  usedPlaces.push(randomPlaceIndex); // Mark chosen place as used
+  currentPlayer = players[i % 2]; // Toggle current player
+  console.log(
+    "Current Player: " +
+      currentPlayer.getName() +
+      ", Type: " +
+      currentPlayer.getType() +
+      ", Mark on: " +
+      randomPlaceIndex +
+      "."
+  );
+  gameBoard.mark(randomPlaceIndex, currentPlayer.getType());
+  i++;
+}
+
+if (win) {
+  console.log("'" + currentPlayer.getName() + "' Win!");
+} else {
+  console.log("Tie!");
+}
