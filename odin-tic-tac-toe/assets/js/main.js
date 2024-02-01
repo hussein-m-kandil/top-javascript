e2dba7ac40ef2c7d1884b2b863b82d709140867e
Hussein Kandil <101815486+hussein-m-kandil@users.noreply.github.com>
2024-02-01 17:38:31 +0200

    Odin Tic Tac Toe: Mark on board & show game result
    Add logic to update the board's cell on click and show dialog that
    announces the game's result (X Win, O Win or Tie).

--- odin-tic-tac-toe/assets/js/main.js
+++ odin-tic-tac-toe/assets/js/main.js
@@ -3,7 +3,8 @@
     const START_EVENT_NAME = "start";
     const WIN_EVENT_NAME = "win";
     const TIE_EVENT_NAME = "tie";
-    const END_EVENT_NAME = "end";
+    const MARK_EVENT_NAME = "mark";
+    const ONE_PLAYER_GAME_EVENT_NAME = "oneplayergame";
     const events = {};
 
     function add(eventName, callbackfn) {
@@ -40,9 +41,10 @@
       remove,
       emit,
       START_EVENT_NAME,
+      ONE_PLAYER_GAME_EVENT_NAME,
       WIN_EVENT_NAME,
+      MARK_EVENT_NAME,
       TIE_EVENT_NAME,
-      END_EVENT_NAME,
     };
   })();
 
@@ -51,7 +53,6 @@
 
     function init() {
       board.fill("", 0);
-      render();
     }
 
     function validateIndex(placeIndex) {
@@ -74,7 +75,7 @@
       }
     }
 
-    function checkForWin() {
+    function checkForWin(type) {
       if (
         (board[0] && board[0] === board[1] && board[1] === board[2]) || // 1st row
         (board[3] && board[3] === board[4] && board[4] === board[5]) || // 2nd row
@@ -85,25 +86,19 @@
         (board[0] && board[0] === board[4] && board[4] === board[8]) || // Diagonal
         (board[6] && board[6] === board[4] && board[4] === board[2]) // Diagonal
       ) {
-        gameEvents.emit(gameEvents.WIN_EVENT_NAME);
+        gameEvents.emit(gameEvents.WIN_EVENT_NAME, type);
       }
     }
 
-    function render() {
-      console.table([
-        [board[0], board[1], board[2]],
-        [board[3], board[4], board[5]],
-        [board[6], board[7], board[8]],
-      ]);
-    }
-
-    function mark(placeIndex, marker) {
+    function mark(placeIndex, type) {
       const i = validateIndex(placeIndex);
       if (i > -1) {
         if (isValidPlace(i)) {
-          board[i] = marker;
-          render();
-          checkForWin();
+          board[i] = type;
+          checkForWin(type);
+          return true;
+        } else {
+          return false;
         }
       }
     }
@@ -112,11 +107,17 @@
   })();
 
   const displayController = (function () {
-    const welcomeDialog = document.createElement("dialog");
+    const dialog = document.createElement("dialog");
 
     function handleNumOfPlayers(event) {
       const numOfPlayers = Number(event.target.value);
       gameEvents.emit(gameEvents.START_EVENT_NAME, numOfPlayers);
+    }
+
+    function createDialogContentDiv() {
+      const dialogContentDiv = document.createElement("div");
+      dialogContentDiv.className = "dialog-content";
+      return dialogContentDiv;
     }
 
     function createChoicesButtons(className, textContentArr, clickHandler) {
@@ -133,51 +134,109 @@
       return buttons;
     }
 
-    function fillWelcomeDialog([firstChoice, secondChoice]) {
-      const welcomeDiv = document.createElement("div");
-      welcomeDiv.className = "welcome";
+    function createWelcomeContent([firstChoice, secondChoice]) {
+      const welcomeDiv = createDialogContentDiv();
       welcomeDiv.append(firstChoice, secondChoice);
-      welcomeDialog.appendChild(welcomeDiv);
+      return welcomeDiv;
     }
 
-    function EmptyWelcomeDialog() {
-      welcomeDialog.childNodes.forEach((child) => child.remove());
+    function fillDialog(content) {
+      dialog.appendChild(content);
     }
 
-    function terminateWelcomeDialog() {
-      welcomeDialog.close();
-      setTimeout(() => welcomeDialog.remove(), 1000);
+    function showDialog() {
+      document.body.appendChild(dialog);
+      dialog.showModal();
+    }
+
+    function emptyDialog() {
+      dialog.childNodes.forEach((child) => child.remove());
+    }
+
+    function terminateDialog() {
+      dialog.close();
+      emptyDialog();
+      setTimeout(() => dialog.remove(), 1000);
     }
 
     function onStart(num) {
       if (num === 1) {
-        EmptyWelcomeDialog();
-        fillWelcomeDialog(
-          createChoicesButtons(
-            "players-type-choice",
-            ["X", "O"],
-            terminateWelcomeDialog
+        emptyDialog();
+        fillDialog(
+          createWelcomeContent(
+            createChoicesButtons("players-type-choice", ["X", "O"], (event) => {
+              terminateDialog();
+              gameEvents.emit(
+                gameEvents.ONE_PLAYER_GAME_EVENT_NAME,
+                event.target.value
+              );
+            })
           )
         );
       } else {
-        terminateWelcomeDialog();
+        terminateDialog();
       }
+    }
+
+    function createMessageDiv(message) {
+      const messageDiv = document.createElement("div");
+      messageDiv.className = "message";
+      messageDiv.textContent = message;
+      return messageDiv;
+    }
+
+    function createDialogCloseButton() {
+      const button = document.createElement("button");
+      button.setAttribute("type", "button");
+      button.className = "dialog-close circle-btn";
+      button.textContent = "x";
+      button.addEventListener("click", terminateDialog);
+      return button;
+    }
+
+    function showMessage(message) {
+      const contentDiv = createDialogContentDiv();
+      contentDiv.append(createDialogCloseButton(), createMessageDiv(message));
+      fillDialog(contentDiv);
+      showDialog();
+    }
+
+    function listenToAllBoardCells() {
+      document.querySelectorAll(".board-cell")?.forEach((cell, cellIndex) =>
+        cell.addEventListener("click", () => {
+          gameEvents.emit(gameEvents.MARK_EVENT_NAME, cell, cellIndex);
+        })
+      );
     }
 
     function init() {
       gameEvents.add(gameEvents.START_EVENT_NAME, onStart);
-      fillWelcomeDialog(
-        createChoicesButtons(
-          "players-num-choice",
-          ["1 Player", "2 Players"],
-          handleNumOfPlayers
+      listenToAllBoardCells();
+      fillDialog(
+        createWelcomeContent(
+          createChoicesButtons(
+            "players-num-choice",
+            ["1 Player", "2 Players"],
+            handleNumOfPlayers
+          )
         )
       );
-      document.body.appendChild(welcomeDialog);
-      welcomeDialog.showModal();
+      showDialog();
     }
 
-    return { init };
+    function mark(cell, type) {
+      cell.textContent = type;
+    }
+
+    function showWinMessage(type) {
+      showMessage("" + String(type).toUpperCase() + " Win!");
+    }
+
+    function showTieMessage(type) {
+      showMessage("Tie!");
+    }
+
+    return { init, mark, showWinMessage, showTieMessage };
   })();
 
   const playerCreator = (function () {
@@ -196,64 +255,114 @@
         return ++score;
       }
 
-      function render() {
-        console.log(type + " Player, Score: " + score);
-      }
-
-      function init() {
-        render();
-      }
-
-      return { init, getType, getScore, incrementScore };
+      return { getType, getScore, incrementScore };
     }
 
     return { create };
   })();
 
-  // Create players
-  const players = [playerCreator.create("X"), playerCreator.create("O")];
-
   // Play a game
-  let numOfPlayers;
-  function onStart(num) {
-    numOfPlayers = num;
-    console.log("" + num + " Player" + (num > 1 ? "s" : "") + " Game.");
-  }
-  gameEvents.add(gameEvents.START_EVENT_NAME, onStart);
-  displayController.init();
-  gameBoard.init(), players[0].init(), players[1].init();
+  const players = [playerCreator.create("X"), playerCreator.create("O")];
+  let numOfPlayers,
+    computerType,
+    gameStarted = false,
+    win = false,
+    roundNum = 0;
 
-  let win = false;
-  function onWin() {
+  function onStart(value) {
+    const num = Number(value);
+    if (!Number.isNaN(num)) {
+      numOfPlayers = num;
+      gameStarted = num === 2;
+    }
+  }
+
+  function onOneGamePlayer(value) {
+    computerType = String(value).toLocaleLowerCase() === "x" ? "O" : "X";
+    gameStarted = true;
+  }
+
+  function onMark(cell, cellIndex) {
+    if (gameStarted) {
+      if (gameBoard.isEmptyPlace()) {
+        let currentPlayerType = players[roundNum % 2].getType();
+        marked = gameBoard.mark(cellIndex, currentPlayerType);
+        if (marked) {
+          roundNum++;
+          displayController.mark(cell, currentPlayerType);
+        }
+      } else {
+        gameEvents.emit(gameEvents.TIE_EVENT_NAME);
+      }
+    }
+  }
+
+  function onWin(type) {
+    setTimeout(() => displayController.showWinMessage(type), 500);
     win = true;
+    gameStarted = false;
   }
+
+  function onTie() {
+    setTimeout(() => displayController.showTieMessage(), 500);
+    gameStarted = false;
+  }
+
+  gameEvents.add(gameEvents.START_EVENT_NAME, onStart);
+  gameEvents.add(gameEvents.ONE_PLAYER_GAME_EVENT_NAME, onOneGamePlayer);
+  gameEvents.add(gameEvents.MARK_EVENT_NAME, onMark);
   gameEvents.add(gameEvents.WIN_EVENT_NAME, onWin);
-  const usedPlaces = [];
-  let currentPlayer,
-    i = 0;
-  while (gameBoard.isEmptyPlace() && !win) {
-    let randomPlaceIndex; // Choose a random, not used place
-    do {
-      randomPlaceIndex = Math.floor(Math.random() * 9);
-    } while (usedPlaces.includes(randomPlaceIndex));
-    usedPlaces.push(randomPlaceIndex); // Mark chosen place as used
-    currentPlayer = players[i % 2]; // Toggle current player
-    console.log(
-      "Current Player: " +
-        currentPlayer.getType() +
-        ", Mark on: " +
-        randomPlaceIndex +
-        "."
-    );
-    gameBoard.mark(randomPlaceIndex, currentPlayer.getType());
-    i++;
+  gameEvents.add(gameEvents.TIE_EVENT_NAME, onTie);
+
+  gameBoard.init(), displayController.init();
+
+  // TEMP CODE...
+
+  const testInterval = setInterval(testGame, 500);
+  function testGame() {
+    if (roundNum === 0) {
+      document
+        .querySelector('dialog>.dialog-content>button[value="2"]')
+        .click();
+    }
+    const usedPlaces = [];
+    if (!win) {
+      let randomPlaceIndex; // Choose a random, not used place
+      do {
+        randomPlaceIndex = Math.floor(Math.random() * 9);
+      } while (usedPlaces.includes(randomPlaceIndex));
+      usedPlaces.push(randomPlaceIndex); // Mark chosen place as used
+      document.querySelectorAll(".board-cell")[randomPlaceIndex].click();
+    } else {
+      clearInterval(testInterval);
+    }
   }
 
-  if (win) {
-    console.log("" + currentPlayer.getType() + " Win!");
-  } else {
-    console.log("Tie!");
-  }
+  // let win = false;
+  // function onWin() {
+  //   win = true;
+  // }
+  // gameEvents.add(gameEvents.WIN_EVENT_NAME, onWin);
+  // const usedPlaces = [];
+  // let currentPlayer,
+  //   i = 0;
+  // if (gameBoard.isEmptyPlace() && !win) {
+  //   let randomPlaceIndex; // Choose a random, not used place
+  //   do {
+  //     randomPlaceIndex = Math.floor(Math.random() * 9);
+  //   } while (usedPlaces.includes(randomPlaceIndex));
+  //   usedPlaces.push(randomPlaceIndex); // Mark chosen place as used
+  //   currentPlayer = players[i % 2]; // Toggle current player
+  //   gameBoard.mark(randomPlaceIndex, currentPlayer.getType());
+  //   document.querySelectorAll(".board-cell")[randomPlaceIndex].click();
+  //   i++;
+  // }
 
-  gameEvents.remove(gameEvents.WIN_EVENT_NAME, onWin);
+  // if (win) {
+  //   console.log("" + currentPlayer.getType() + " Win!");
+  // } else {
+  //   console.log("Tie!");
+  // }
+
+  // gameEvents.remove(gameEvents.WIN_EVENT_NAME, onWin);
 })();
