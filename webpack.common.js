044b5b0be0f4c079dcc899a277c1319b3ba8ec5f
Hussein Kandil <101815486+hussein-m-kandil@users.noreply.github.com>
2024-02-21 13:01:21 +0200

    Transpile js code with babel

--- webpack.common.js
+++ webpack.common.js
@@ -33,8 +33,19 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.css$/i,
+        test: /\.css$/,
         use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(?:js|mjs|cjs)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: [["@babel/preset-env", { targets: "defaults" }]],
+            plugins: ["@babel/plugin-transform-class-properties"],
+          },
+        },
       },
     ],
   },
