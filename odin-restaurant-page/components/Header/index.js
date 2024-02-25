3a3631b2c531573aef3e68bcedb62a3057ab1f89
Hussein Kandil <101815486+hussein-m-kandil@users.noreply.github.com>
2024-02-25 20:04:29 +0200

    Odin Restaurant Page: Add Header's buttons onClick

--- odin-restaurant-page/components/Header/index.js
+++ odin-restaurant-page/components/Header/index.js
@@ -2,29 +2,33 @@ import "./index.css";
 
 import createElement from "../../helpers/createElement.js";
 
-const header = createElement("header", "header");
-header.appendChild(createElement("h1", "head", null, "Odin Restaurant"));
-// Main Menu
-const menuButtonsData = [
-  { className: "nav-btn home-btn", textContent: "Home" },
-  { className: "nav-btn menu-btn", textContent: "Menu" },
-  { className: "nav-btn contact-btn", textContent: "Contact" },
-];
-const nav = createElement("nav", "nav-menu");
-const navList = createElement("ul", "nav-ul");
-for (let i = 0; i < menuButtonsData.length; i++) {
-  const navLi = createElement("li", "nav-li");
-  navLi.appendChild(
-    createElement(
+/**
+ *
+ * @param {Object<string, any>} props
+ * @returns {HTMLElement}
+ */
+export default function Header({ headTextContent, menuButtonsData }) {
+  const header = createElement("header", "header");
+  header.appendChild(createElement("h1", "head", headTextContent));
+  // Main Menu
+  const nav = createElement("nav", "nav-menu");
+  const navList = createElement("ul", "nav-ul");
+  for (let i = 0; i < menuButtonsData.length; i++) {
+    const navLi = createElement("li", "nav-li");
+    const navBtn = createElement(
       "button",
       menuButtonsData[i].className,
-      null,
       menuButtonsData[i].textContent,
       ["type", "button"]
-    )
-  );
-  navList.appendChild(navLi);
+    );
+    if (menuButtonsData[i].onClick) {
+      navBtn.addEventListener("click", menuButtonsData[i].onClick);
+    }
+    navLi.appendChild(navBtn);
+    navList.appendChild(navLi);
+  }
+  nav.appendChild(navList), header.appendChild(nav);
+  return header;
 }
-nav.appendChild(navList), header.appendChild(nav);
 
-export default header;
+export { Header };
