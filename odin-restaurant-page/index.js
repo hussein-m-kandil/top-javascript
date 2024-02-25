da684f13cfdd25a148b1fb05ed3ee91d8c7acec1
Hussein Kandil <101815486+hussein-m-kandil@users.noreply.github.com>
2024-02-25 20:08:23 +0200

    Odin Restaurant Page: Plug most of the components
    Import most of the needed components in the app's main module
    and initiate the logic to insert content components on demand.

--- odin-restaurant-page/index.js
+++ odin-restaurant-page/index.js
@@ -1,8 +1,56 @@
 import "./index.css";
 
+import createElement from "./helpers/createElement";
 import Header from "./components/Header";
+import Home from "./components/Home";
+import Menu from "./components/Menu";
+import Contact from "./components/Contact";
 
-document.body.appendChild(Header);
-// TODO: Make a module that create the contents
-// and probably you will need a system of events
-// for the app's components to interact with each other.
+const CONTENT_DIV_CLASS_NAME = "content-container";
+
+function getHomeContent() {
+  document.querySelector("." + CONTENT_DIV_CLASS_NAME)?.remove();
+  // TODO: Try to delay the process a while and display loading GIF ;)
+  const contentDiv = createElement("div", CONTENT_DIV_CLASS_NAME);
+  contentDiv.appendChild(Home());
+  document.body.appendChild(contentDiv);
+}
+
+function getMenuContent() {
+  document.querySelector("." + CONTENT_DIV_CLASS_NAME)?.remove();
+  // TODO: Try to delay the process a while and display loading GIF ;)
+  const contentDiv = createElement("div", CONTENT_DIV_CLASS_NAME);
+  contentDiv.appendChild(Menu());
+  document.body.appendChild(contentDiv);
+}
+
+function getContactContent() {
+  document.querySelector("." + CONTENT_DIV_CLASS_NAME)?.remove();
+  // TODO: Try to delay the process a while and display loading GIF ;)
+  const contentDiv = createElement("div", CONTENT_DIV_CLASS_NAME);
+  contentDiv.appendChild(Contact());
+  document.body.appendChild(contentDiv);
+}
+
+document.body.appendChild(
+  Header({
+    headTextContent: "Odin Restaurant",
+    menuButtonsData: [
+      {
+        className: "nav-btn home-btn",
+        textContent: "Home",
+        onClick: getHomeContent,
+      },
+      {
+        className: "nav-btn menu-btn",
+        textContent: "Menu",
+        onClick: getMenuContent,
+      },
+      {
+        className: "nav-btn contact-btn",
+        textContent: "Contact",
+        onClick: getContactContent,
+      },
+    ],
+  })
+);
