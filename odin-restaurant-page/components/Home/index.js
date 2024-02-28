b0768ae586df4025513d32f99854477bb2b32f60
Hussein Kandil <101815486+hussein-m-kandil@users.noreply.github.com>
2024-02-28 14:10:36 +0200

    Odin Restaurant Page: Add Home contents & styles

--- odin-restaurant-page/components/Home/index.js
+++ odin-restaurant-page/components/Home/index.js
@@ -1,6 +1,7 @@
 import "./index.css";
 
 import createElement from "../../helpers/createElement.js";
+import heroImageSrc from "./assets/images/food-painted-egg-plate.jpg";
 
 /**
  *
@@ -8,12 +9,22 @@ import createElement from "../../helpers/createElement.js";
  * @returns {HTMLElement}
  */
 export default function Home() {
-  const home = createElement(
-    "div",
-    "home-content",
-    "This is the content of the home page!"
-  );
+  const home = createElement("div", "home");
 
+  const hero = createElement("div", "home-hero");
+  const heroImage = new Image(100, 100);
+  heroImage.src = heroImageSrc;
+  heroImage.className = "hero-image";
+  hero.appendChild(heroImage);
+
+  const heroText = createElement("div", "hero-text");
+  const heroTextHead = createElement("div", "hero-text-head", "Magical");
+  const heroTextBody = createElement("div", "hero-text-body", "healthy food");
+  heroText.appendChild(heroTextHead),
+    heroText.appendChild(heroTextBody),
+    hero.appendChild(heroText);
+
+  home.appendChild(hero);
   return home;
 }
 
