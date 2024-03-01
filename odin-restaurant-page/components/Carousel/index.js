cc5aec100f051a266623bd87ffa604d7bffbbc28
Hussein Kandil <101815486+hussein-m-kandil@users.noreply.github.com>
2024-03-01 14:27:15 +0200

    Odin Restaurant Page: Animate Carousel

--- odin-restaurant-page/components/Carousel/index.js
+++ odin-restaurant-page/components/Carousel/index.js
@@ -6,32 +6,58 @@ import imageSrc1 from "./assets/images/ai-generated-8571703_1280.jpg";
 import imageSrc2 from "./assets/images/ai-generated-8591924_1280.jpg";
 
 export default function Carousel() {
+  const RIGHT_SLIDE_IN = "right-slide-in";
+  const RIGHT_SLIDE_OUT = "right-slide-out";
   const carousel = createElement("div", "carousel");
 
-  const imageSources = [imageSrc1, imageSrc2];
+  const imgSrcArr = [imageSrc1, imageSrc2]; // TODO: use dynamic 'import()' here
+  let imgSrcIndex = 0;
 
-  function makeImage(src) {
+  const makeImage = (src) => {
     const image = new Image(200);
     image.src = src;
     image.className = "carousel-image";
     return image;
-  }
+  };
 
-  const figure = createElement("figure", "carousel-figure");
-  figure.appendChild(makeImage(imageSources[0]));
-  const figcaption = createElement("figcaption", "carousel-figcaption");
-  figcaption.appendChild(
-    createCreditsDiv(
-      "Image by ",
-      "Kalpesh Ajugia",
-      "https://pixabay.com/users/pixellicious-13377274/",
-      " from ",
-      "Pixabay",
-      "https://pixabay.com/"
-    )
-  );
-  figure.appendChild(figcaption);
-  carousel.appendChild(figure);
+  const getImageSrc = () => {
+    const src = imgSrcArr[imgSrcIndex];
+    imgSrcIndex = imgSrcIndex + 1 >= imgSrcArr.length ? 0 : ++imgSrcIndex;
+    return src;
+  };
+
+  const appendNewImageFigure = (newImage) => {
+    const figure = createElement("figure", "carousel-figure");
+    figure.appendChild(newImage);
+    const figcaption = createElement("figcaption", "carousel-figcaption");
+    figcaption.appendChild(
+      createCreditsDiv(
+        "Image by ",
+        "Kalpesh Ajugia",
+        "https://pixabay.com/users/pixellicious-13377274/",
+        " from ",
+        "Pixabay",
+        "https://pixabay.com/"
+      )
+    );
+    figure.appendChild(figcaption);
+    figure.classList.add("right-slide-in");
+    carousel.appendChild(figure);
+  };
+
+  carousel.addEventListener("animationend", (event) => {
+    // TODO: Try to use animation directly without classes.
+    if (event.animationName === RIGHT_SLIDE_OUT) {
+      event.target.remove();
+      appendNewImageFigure(makeImage(getImageSrc()));
+    } else if (event.animationName === RIGHT_SLIDE_IN) {
+      setTimeout(() => {
+        event.target?.classList.replace(RIGHT_SLIDE_IN, RIGHT_SLIDE_OUT);
+      }, 3000);
+    }
+  });
+
+  appendNewImageFigure(makeImage(getImageSrc()));
 
   return carousel;
 }
