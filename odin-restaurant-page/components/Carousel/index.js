317d1f7db2e5cbb3031b0eb1e9f2e3399c642a4a
Hussein Kandil <101815486+hussein-m-kandil@users.noreply.github.com>
2024-03-03 15:05:37 +0200

    Odin Restaurant Page: Control Carousel play&motion

--- odin-restaurant-page/components/Carousel/index.js
+++ odin-restaurant-page/components/Carousel/index.js
@@ -1,18 +1,33 @@
 import "./index.css";
 
 import createElement from "../../helpers/createElement.js";
-import createCreditsDiv from "../../helpers/createCreditsDiv.js";
 import imageSrc1 from "./assets/images/ai-generated-8571703_1280.jpg";
 import imageSrc2 from "./assets/images/ai-generated-8591924_1280.jpg";
 
 // TODO: Document the needed props
 export default function Carousel() {
-  const RIGHT_SLIDE_IN = "right-slide-in";
-  const RIGHT_SLIDE_OUT = "right-slide-out";
+  const SLIDE_INTERVAL = 5000;
+  const ANIMATION_DURATION = 750;
+  const LEFT_SLIDE_IN_STYLE = " animation-name: left-slide-in; ";
+  const LEFT_SLIDE_OUT_STYLE = " animation-name: left-slide-out; ";
+  const RIGHT_SLIDE_IN_STYLE = " animation-name: right-slide-in; ";
+  const RIGHT_SLIDE_OUT_STYLE = " animation-name: right-slide-out; ";
+  const ANIMATION_STYLES =
+    " animation-duration: " +
+    ANIMATION_DURATION +
+    "ms; " +
+    " animation-iteration-count: 1; " +
+    " animation-fill-mode: forwards; " +
+    " animation-timing-function: ease-in-out; ";
   const carousel = createElement("div", "carousel");
   const figure = createElement("figure", "carousel-figure");
   const leftArrow = createElement("div", "carousel-left-arrow");
   const rightArrow = createElement("div", "carousel-right-arrow");
+  const controllers = createElement("div", "carousel-controllers");
+  const playBtn = createElement("div", "carousel-play-btn", "▶");
+  const imgCircles = createElement("div", "carousel-img-circles");
+  const animationBtn = createElement("div", "carousel-animation-btn", "▶▶");
+  controllers.append(playBtn, imgCircles, animationBtn);
   // TODO: use dynamic 'import()' here
   const images = [
     {
@@ -43,7 +58,9 @@ export default function Carousel() {
     },
   ];
   let currentImageIndex = 0,
-    slideOutTimeout = 0;
+    slideInterval = 0,
+    animate = true,
+    backward = false;
 
   const makeNewImage = (src) => {
     const image = new Image(200);
@@ -55,16 +72,32 @@ export default function Carousel() {
 
   const makeNewCaption = (imgCaptionData) => {
     const figcaption = createElement("figcaption", "carousel-figcaption");
-    figcaption.appendChild(
-      createCreditsDiv(
-        imgCaptionData.owner.name ? "Image by " : null,
-        imgCaptionData.owner.name,
-        imgCaptionData.owner.url,
-        imgCaptionData.site.name ? " from " : null,
-        imgCaptionData.site.name,
-        imgCaptionData.site.url
-      )
-    );
+    if (imgCaptionData.owner.name && imgCaptionData.owner.url) {
+      figcaption.appendChild(document.createTextNode("Image by "));
+      figcaption.appendChild(
+        createElement("a", "credits-owner", imgCaptionData.owner.name, [
+          "href",
+          imgCaptionData.owner.url,
+        ])
+      );
+    } else if (imgCaptionData.owner.name) {
+      figcaption.appendChild(
+        document.createTextNode("Image by " + imgCaptionData.owner.name)
+      );
+    }
+    if (imgCaptionData.site.name && imgCaptionData.site.url) {
+      figcaption.appendChild(document.createTextNode(" from "));
+      figcaption.appendChild(
+        createElement("a", "credits-site", imgCaptionData.site.name, [
+          "href",
+          imgCaptionData.site.url,
+        ])
+      );
+    } else if (imgCaptionData.site.name) {
+      figcaption.appendChild(
+        document.createTextNode(" from " + imgCaptionData.site.name)
+      );
+    }
     return figcaption;
   };
 
@@ -84,6 +117,22 @@ export default function Carousel() {
     }
   };
 
+  const updateImageCircles = () => {
+    [...imgCircles.children].forEach((circle, i) => {
+      if (
+        currentImageIndex === i &&
+        !circle.classList.contains("carousel-img-circle-active")
+      ) {
+        circle.classList.add("carousel-img-circle-active");
+      } else if (
+        currentImageIndex !== i &&
+        circle.classList.contains("carousel-img-circle-active")
+      ) {
+        circle.classList.remove("carousel-img-circle-active");
+      }
+    });
+  };
+
   const updateImageFigure = () => {
     const imgSrc = images[currentImageIndex].src;
     const imgCaptionData = images[currentImageIndex].caption;
@@ -92,44 +141,112 @@ export default function Carousel() {
     if (imgCaptionData) {
       figure.appendChild(makeNewCaption(imgCaptionData));
     }
-    figure.style.animationName = RIGHT_SLIDE_IN;
+    if (animate) {
+      if (backward) {
+        figure.setAttribute("style", LEFT_SLIDE_IN_STYLE + ANIMATION_STYLES);
+      } else {
+        figure.setAttribute("style", RIGHT_SLIDE_IN_STYLE + ANIMATION_STYLES);
+      }
+    } else {
+      figure.removeAttribute("style");
+    }
   };
 
   const showNewImage = () => {
-    // Assert that is already in the carousel
-    if (figure.parentElement === carousel) {
-      carousel.removeChild(figure);
+    if (backward) {
+      decrementImageIndex();
+    } else {
+      incrementImageIndex();
     }
     updateImageFigure();
-    carousel.appendChild(figure);
+    updateImageCircles();
+    backward = false;
   };
 
-  carousel.addEventListener("animationend", (event) => {
-    if (event.animationName === RIGHT_SLIDE_OUT) {
-      incrementImageIndex();
-      showNewImage();
-    } else if (event.animationName === RIGHT_SLIDE_IN) {
-      if (slideOutTimeout) {
-        clearTimeout(slideOutTimeout);
+  const slide = () => {
+    if (animate) {
+      if (backward) {
+        figure.setAttribute("style", LEFT_SLIDE_OUT_STYLE + ANIMATION_STYLES);
+      } else {
+        figure.setAttribute("style", RIGHT_SLIDE_OUT_STYLE + ANIMATION_STYLES);
       }
-      slideOutTimeout = setTimeout(() => {
-        figure.style.animationName = RIGHT_SLIDE_OUT;
-      }, 5000);
+      setTimeout(showNewImage, ANIMATION_DURATION);
+    } else {
+      figure.removeAttribute("style");
+      showNewImage();
     }
-  });
+  };
 
-  leftArrow.addEventListener("click", () => {
-    decrementImageIndex();
-    showNewImage();
-  });
+  const slideManually = () => {
+    if (slideInterval) {
+      clearInterval(slideInterval);
+      slideInterval = 0;
+    }
+    slide();
+    slideInterval = setInterval(slide, SLIDE_INTERVAL);
+  };
 
-  rightArrow.addEventListener("click", () => {
-    incrementImageIndex();
-    showNewImage();
-  });
+  leftArrow.addEventListener(
+    "click",
+    () => {
+      backward = true;
+      slideManually();
+    },
+    false
+  );
 
-  carousel.append(leftArrow, rightArrow);
-  showNewImage();
+  rightArrow.addEventListener(
+    "click",
+    () => {
+      backward = false;
+      slideManually();
+    },
+    false
+  );
+
+  playBtn.addEventListener(
+    "click",
+    () => {
+      if (slideInterval) {
+        clearInterval(slideInterval);
+        slideInterval = 0;
+      } else {
+        slideInterval = setInterval(slide, SLIDE_INTERVAL);
+      }
+      playBtn.classList.toggle("carousel-play-btn-active");
+    },
+    false
+  );
+
+  animationBtn.addEventListener(
+    "click",
+    () => {
+      animate = !animate;
+      animationBtn.classList.toggle("carousel-animation-btn-active");
+    },
+    false
+  );
+
+  carousel.append(leftArrow, rightArrow, figure);
+  for (let i = 0; i < images.length; i++) {
+    const imgCircle = createElement("div", "img-circle");
+    imgCircle.addEventListener(
+      "click",
+      () => {
+        currentImageIndex = i;
+        updateImageFigure();
+        updateImageCircles();
+      },
+      false
+    );
+    imgCircles.appendChild(imgCircle);
+  }
+  carousel.appendChild(controllers);
+
+  updateImageFigure();
+  updateImageCircles();
+
+  slideInterval = setInterval(slide, SLIDE_INTERVAL);
 
   return carousel;
 }
