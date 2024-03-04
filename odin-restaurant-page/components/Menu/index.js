2be2e0037d394feee9e35d332be88bfad3982b8f
Hussein Kandil <101815486+hussein-m-kandil@users.noreply.github.com>
2024-03-04 16:43:50 +0200

    Odin Restaurant Page: Clear menu duplicated images

--- odin-restaurant-page/components/Menu/index.js
+++ odin-restaurant-page/components/Menu/index.js
@@ -2,20 +2,18 @@ import "./index.css";
 
 import createElement from "../../helpers/createElement.js";
 import Carousel from "../Carousel/index.js";
-import imageSrc01 from "./assets/images/ai-generated-8571703_1280.jpg";
-import imageSrc02 from "./assets/images/ai-generated-8591924_1280.jpg";
-import imageSrc03 from "./assets/images/ai-generated-8022803_1280.jpg";
-import imageSrc04 from "./assets/images/ai-generated-8022804_1280.png";
-import imageSrc05 from "./assets/images/ai-generated-8176698_1280.png";
-import imageSrc06 from "./assets/images/ai-generated-8270741_1280.jpg";
-import imageSrc07 from "./assets/images/ai-generated-8489077_1280.jpg";
-import imageSrc08 from "./assets/images/ai-generated-8571703_1280.jpg";
-import imageSrc09 from "./assets/images/ai-generated-8582529_1280.jpg";
-import imageSrc10 from "./assets/images/ai-generated-8591924_1280.jpg";
-import imageSrc11 from "./assets/images/chicken-8063711_1280.png";
-import imageSrc12 from "./assets/images/cold-8255451_1280.jpg";
-import imageSrc13 from "./assets/images/noodles-8174666_1280.png";
-import imageSrc14 from "./assets/images/steak-8063731_1280.png";
+import imageSrc01 from "./assets/images/ai-generated-8022803_1280.jpg";
+import imageSrc02 from "./assets/images/ai-generated-8022804_1280.png";
+import imageSrc03 from "./assets/images/ai-generated-8176698_1280.png";
+import imageSrc04 from "./assets/images/ai-generated-8270741_1280.jpg";
+import imageSrc05 from "./assets/images/ai-generated-8489077_1280.jpg";
+import imageSrc06 from "./assets/images/ai-generated-8571703_1280.jpg";
+import imageSrc07 from "./assets/images/ai-generated-8582529_1280.jpg";
+import imageSrc08 from "./assets/images/ai-generated-8591924_1280.jpg";
+import imageSrc09 from "./assets/images/chicken-8063711_1280.png";
+import imageSrc10 from "./assets/images/cold-8255451_1280.jpg";
+import imageSrc11 from "./assets/images/noodles-8174666_1280.png";
+import imageSrc12 from "./assets/images/steak-8063731_1280.png";
 
 /**
  *
@@ -38,8 +36,6 @@ export default function Menu() {
     imageSrc10,
     imageSrc11,
     imageSrc12,
-    imageSrc13,
-    imageSrc14,
   ];
   const overallImgAlt = "AI generated image of food.";
   const overallImgCaptionData = {
