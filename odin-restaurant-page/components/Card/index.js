6a05346899f9e01ec1223997f9a9ee1c070c5102
Hussein Kandil <101815486+hussein-m-kandil@users.noreply.github.com>
2024-03-06 07:51:16 +0200

    Odin Restaurant Page: Document Card component

--- odin-restaurant-page/components/Card/index.js
+++ odin-restaurant-page/components/Card/index.js
@@ -6,20 +6,24 @@ import createFigCaption from "../../helpers/createFigCaption.js";
 // TODO: Document Card component
 /**
  * Image carousel component.
- * @param {Array<{
- *  image: HTMLImageElement,
- *  captionData: {
- *    name: { name: string?, url: string? }?,
- *    site: { name: string?, url: string? }?
- *   }?
- * }>} images
- * Array of objects, each of which has 'image' key (type 'Image')
- * and (optional) 'captionData' (type 'Object').
- * - 'captionData' is {owner:{name,url},site:{name, url}} (notice the keys' names).
- * - 'captionData' produces an image's caption like "Image by owner.name from site.name".
- * - 'captionData' produces the names as '<a>' the 'url' key is present.
+ * @param {{
+ *  cardTitle: string?,
+ *  image: HTMLImageElement?,
+ *  cardImageWithCaption: {
+ *    image: HTMLImageElement?,
+ *    captionData: {
+ *      owner: { name: string?, url: string? }?,
+ *      site: { name: string?, url: string? }?
+ *  }?},
+ *  cardBody: string?
+ * }?} props
+ * Object that has some data for the card, like:
+ * - cardTitle: string
+ * - image: HTMLImageElement
+ * - cardImageWithCaption: {image, captionData: {owner: {name, url}, site: {name, url}}}
+ * - cardBody: string
  *
- * Note: The 'captionData' key and all its nested keys are optional.
+ * Note: All properties are optional.
  * @returns {HTMLDivElement}
  */
 export default function Card(props) {
