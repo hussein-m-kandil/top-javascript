8a91517054a21e2683169df770ae9278456441c2
Hussein Kandil <101815486+hussein-m-kandil@users.noreply.github.com>
2024-03-08 07:05:06 +0200

    Odin Restaurant Page: Remove Footer & Header fixed

--- odin-restaurant-page/index.js
+++ odin-restaurant-page/index.js
@@ -3,7 +3,6 @@ import "./index.css";
 import createElement from "./helpers/createElement.js";
 import titleizeHash from "./helpers/titleizeHash.js";
 import Header from "./components/Header";
-import Footer from "./components/Footer";
 import Home from "./components/Home";
 import Menu from "./components/Menu";
 import Contact from "./components/Contact";
@@ -40,33 +39,8 @@ const header = Header({
 document.body.appendChild(header);
 
 const contentDiv = createElement("div", "content-container");
+contentDiv.style.top = `${header.offsetHeight}px`;
 document.body.appendChild(contentDiv);
-
-const footer = Footer(MENU_BUTTONS_DATA);
-let footerAdded = false;
-
-// Observe the header intersection with viewport to know when to add the fixed footer
-if (IntersectionObserver) {
-  const intersectionObserver = new IntersectionObserver((entries, observer) => {
-    setTimeout(() => {
-      const headerEntry = entries.at(0);
-      console.log("Is header visible? " + headerEntry.isIntersecting);
-      if (!headerEntry.isIntersecting) {
-        document.body.appendChild(footer);
-        document.body.style.paddingBottom = `${footer.offsetHeight}px`;
-        footerAdded = true;
-      } else {
-        if (footerAdded) {
-          document.body.removeChild(footer);
-          document.body.style.paddingBottom = "0px";
-          footerAdded = false;
-        }
-      }
-    }, 0);
-  });
-  intersectionObserver.observe(header);
-}
-// TODO: Add another observation method in case IntersectionObservation not supported
 
 /**
  * Loads the correct content based on 'window.location.hash' or load 'Home' content.
