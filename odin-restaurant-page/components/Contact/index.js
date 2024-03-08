9029ce24124e0bb572cadb07501786e224118e62
Hussein Kandil <101815486+hussein-m-kandil@users.noreply.github.com>
2024-03-08 13:49:19 +0200

    Odin Restaurant Page: Fill Contact component

--- odin-restaurant-page/components/Contact/index.js
+++ odin-restaurant-page/components/Contact/index.js
@@ -1,18 +1,41 @@
 import "./index.css";
 
 import createElement from "../../helpers/createElement.js";
+import createCredits from "../../helpers/createCredits.js";
 
 /**
  *
- * @param {Object<string, any>} props
  * @returns {HTMLElement}
  */
 export default function Contact() {
-  const contact = createElement(
-    "div",
-    "contact-content",
-    "This is the content of the contact page!"
+  const contact = createElement("div", "contact");
+
+  const contactEntries = createElement("div", "contact-entries");
+  const tel = createElement("div", "tel", "Tel ");
+  tel.appendChild(createElement("span", "tel-span", "00 000 000"));
+  const address = createElement("div", "address", "Address ");
+  address.appendChild(
+    createElement(
+      "span",
+      "address-span",
+      "7 Blah blah st, Earth Planet, Universe."
+    )
   );
+  const email = createElement("div", "email", "E-mail ");
+  email.appendChild(
+    createElement("span", "email-span", "odinrestaurant@example.com")
+  );
+  const website = createElement("div", "website", "Website ");
+  website.appendChild(
+    createElement(
+      "span",
+      "website-span",
+      "" + (window.location.href?.replace(/#.+$/, "") ?? "example.com")
+    )
+  );
+
+  contactEntries.append(tel, address, email, website);
+  contact.appendChild(contactEntries);
 
   return contact;
 }
