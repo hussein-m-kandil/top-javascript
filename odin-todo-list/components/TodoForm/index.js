4413cbed38a18af31e62a866b94829fecd07629e
Hussein Kandil <101815486+hussein-m-kandil@users.noreply.github.com>
2024-03-13 17:54:28 +0200

    Odin Todo List: Add needed fields in TodoForm

--- odin-todo-list/components/TodoForm/index.js
+++ odin-todo-list/components/TodoForm/index.js
@@ -1,8 +1,12 @@
 import "./index.css";
 
+import { format as formatDate, isEqual, isAfter } from "date-fns";
 import createElement from "../../helpers/createElement.js";
 
 export default function TodoForm() {
+  // Current time
+  const currentDateTime = formatDate(new Date(), "yyyy-MM-dd'T'HH:mm");
+
   // Form
   const todoForm = createElement(
     "form",
@@ -13,37 +17,153 @@ export default function TodoForm() {
   );
 
   // Title
-  const titleDiv = createElement("div", "todo-title");
-  const titleLabel = createElement("label", "title-label", "Title ", [
+  const titleDiv = createElement("div", "title");
+  const titleLabel = createElement("label", "title", "Title ", [
     "for",
-    "title-input",
+    "title",
   ]);
   const titleInput = createElement(
     "input",
-    "title-input",
+    "title",
     null,
-    ["id", "title-input"],
-    ["type", "text"]
+    ["id", "title"],
+    ["name", "title"],
+    ["type", "text"],
+    ["autocomplete", "on"],
+    ["autofocus", ""],
+    ["required", ""]
   );
   titleDiv.append(titleLabel, titleInput);
 
-  // Submit
-  const submitDiv = createElement("div", "todo-submit");
-  const submitButton = createElement(
-    "button",
-    "submit-button",
-    "Create New Todo",
-    ["type", "submit"]
+  // Description
+  const descriptionDiv = createElement("div", "description");
+  const descriptionLabel = createElement(
+    "label",
+    "description",
+    "Description ",
+    ["for", "description"]
   );
+  const descriptionText = createElement(
+    "textarea",
+    "description",
+    null,
+    ["id", "description"],
+    ["name", "description"]
+  );
+  descriptionDiv.append(descriptionLabel, descriptionText);
+
+  // Due date
+  const dueDateDiv = createElement("div", "due-date");
+  const dueDateLabel = createElement("label", "due-date", "Due Date ", [
+    "for",
+    "due-date",
+  ]);
+  const dueDateInput = createElement(
+    "input",
+    "due-date",
+    null,
+    ["id", "due-date"],
+    ["name", "due-date"],
+    ["type", "datetime-local"],
+    ["min", currentDateTime],
+    ["required", ""]
+  );
+  dueDateDiv.append(dueDateLabel, dueDateInput);
+
+  // Priority
+  const priorityFieldset = createElement("fieldset", "priority");
+  const priorityLegend = createElement("legend", "priority", "Priority ");
+  const highPriorityDiv = createElement("div", "priority");
+  const highPriorityLabel = createElement("label", "priority", "High ", [
+    "for",
+    "high-priority",
+  ]);
+  const highPriorityRadio = createElement(
+    "input",
+    "priority",
+    null,
+    ["id", "high-priority"],
+    ["name", "priority"],
+    ["type", "radio"]
+  );
+  highPriorityDiv.append(highPriorityRadio, highPriorityLabel);
+  const mediumPriorityDiv = createElement("div", "priority");
+  const mediumPriorityLabel = createElement("label", "priority", "Medium ", [
+    "for",
+    "medium-priority",
+  ]);
+  const mediumPriorityRadio = createElement(
+    "input",
+    "priority",
+    null,
+    ["type", "radio"],
+    ["id", "medium-priority"],
+    ["name", "priority"],
+    ["checked", ""]
+  );
+  mediumPriorityDiv.append(mediumPriorityRadio, mediumPriorityLabel);
+  const lowPriorityDiv = createElement("div", "priority");
+  const lowPriorityLabel = createElement("label", "priority", "Low ", [
+    "for",
+    "low-priority",
+  ]);
+  const lowPriorityRadio = createElement(
+    "input",
+    "priority",
+    null,
+    ["id", "low-priority"],
+    ["name", "priority"],
+    ["type", "radio"]
+  );
+  lowPriorityDiv.append(lowPriorityRadio, lowPriorityLabel);
+  priorityFieldset.append(
+    priorityLegend,
+    highPriorityDiv,
+    mediumPriorityDiv,
+    lowPriorityDiv
+  );
+
+  // Submit
+  const submitDiv = createElement("div", "submit");
+  const submitButton = createElement("button", "submit", "Create New Todo", [
+    "type",
+    "submit",
+  ]);
   submitDiv.appendChild(submitButton);
 
   // Append all controls to form
-  todoForm.append(titleDiv, submitDiv);
+  todoForm.append(
+    titleDiv,
+    descriptionDiv,
+    dueDateDiv,
+    priorityFieldset,
+    submitDiv
+  );
 
   // Handle form submission
   todoForm.addEventListener("submit", (event) => {
     event.preventDefault();
-    console.log("Form submitted!");
+    const form = event.target;
+    // Validate title field
+    if (!form["title"].value) {
+      form["title"].setCustomValidity("Todo's title cannot be empty!");
+      form["title"].addEventListener("input", () => {
+        form["title"].setCustomValidity("");
+      });
+    }
+    // Validate due date field
+    if (
+      !isEqual(new Date(form["due-date"].value), new Date(currentDateTime)) &&
+      !isAfter(new Date(form["due-date"].value), new Date(currentDateTime))
+    ) {
+      form["due-date"].setCustomValidity(
+        "The date/time must be after or equal to the current date/time!"
+      );
+      form["due-date"].addEventListener("change", () => {
+        form["due-date"].setCustomValidity("");
+      });
+    }
+    // TODO: Create new Todo ;)
   });
 
   return todoForm;
