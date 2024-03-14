df5421a9c203c79e62522dad7a6c1335520f2e46
Hussein Kandil <101815486+hussein-m-kandil@users.noreply.github.com>
2024-03-14 17:50:53 +0200

    Odin Todo List: Add button to toggle TodoForm

--- odin-todo-list/index.js
+++ odin-todo-list/index.js
@@ -3,11 +3,45 @@ import "./index.css";
 import createElement from "./helpers/createElement.js";
 import TodoListEvents from "./helpers/TodoListEvents.js";
 import TodoForm from "./components/TodoForm";
+import Button from "./components/Button";
 
-document.body.appendChild(createElement("h1", "head", "Odin Todo List"));
+const todoList = [];
+let newTodoFormPresented = false;
 
-document.body.appendChild(TodoForm());
+const header = createElement("header", "todo-header");
+header.appendChild(createElement("h1", "todo-head", "Odin Todo List"));
 
-TodoListEvents.add(TodoListEvents.CREATE_NEW_TODO, (data) => {
-  console.log("Create new todo using these data => ", data);
+const main = createElement("main");
+const emptyMain = () => {
+  [...main.children].forEach((node) => main.removeChild(node));
+};
+
+const newTodoButton = Button({
+  className: "new-todo",
+  type: "button",
+  textContent: "Add New Todo",
 });
+newTodoButton.addEventListener("click", () => {
+  emptyMain();
+  if (!newTodoFormPresented) {
+    main.appendChild(TodoForm());
+    newTodoButton.textContent = "Home";
+    newTodoFormPresented = true;
+  } else {
+    // TODO: Fill page with todos
+    newTodoFormPresented = false;
+    newTodoButton.textContent = "Add New Todo";
+  }
+});
+
+header.appendChild(newTodoButton);
+
+TodoListEvents.add(TodoListEvents.CREATE_NEW_TODO, (todoInfo) => {
+  todoList.push(todoInfo);
+  emptyMain();
+  // TODO: Fill page with todos
+});
+
+document.body.append(header, main);
+// Calc main's top margin = header's height + 1rem
+main.setAttribute("style", `marginTop: calc(${header.offsetHeight}px + 1rem)`);
