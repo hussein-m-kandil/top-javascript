3165897853c12a56d5d38bf64bfe8bcace2cbc9b
Hussein Kandil <101815486+hussein-m-kandil@users.noreply.github.com>
2024-03-15 15:26:32 +0200

    Odin Todo List: Format TodoCard dates & add styles

--- odin-todo-list/components/TodoCard/index.js
+++ odin-todo-list/components/TodoCard/index.js
@@ -1,6 +1,15 @@
 import "./index.css";
 
 import createElement from "../../helpers/createElement.js";
+import capitalize from "../../helpers/capitalize.js";
+import {
+  isDate,
+  isToday,
+  isTomorrow,
+  isPast,
+  formatDate,
+  formatDistanceToNow,
+} from "date-fns";
 
 /**
  *
@@ -13,25 +22,45 @@ import createElement from "../../helpers/createElement.js";
  * @returns {HTMLDivElement}
  */
 export default function TodoCard(props) {
+  // Prepare todo due date
+  let overdue = false;
+  let dueDateText = "Due ";
+  if (isDate(props.dueDate)) {
+    if (isPast(props.dueDate, new Date())) {
+      overdue = true;
+      dueDateText = "" + formatDistanceToNow(props.dueDate) + " Overdue!";
+    } else if (isToday(props.dueDate)) {
+      dueDateText += "today at " + formatDate(props.dueDate, "hh:mm a");
+    } else if (isTomorrow(props.dueDate)) {
+      dueDateText += "tomorrow at " + formatDate(props.dueDate, "hh:mm a");
+    } else {
+      dueDateText += formatDistanceToNow(props.dueDate);
+    }
+  } else {
+    dueDateText += props.dueDate;
+  }
+
+  // Create card elements
   const todoCard = createElement("div", "todo-card");
-  const title = createElement("div", "title", "Title: " + props.title);
+  const title = createElement("div", "title", props.title);
+  const dueDate = createElement(
+    "div",
+    "due-date" + (overdue ? " overdue" : ""),
+    dueDateText
+  );
   const description = createElement(
     "div",
     "description",
-    "Description: " + props.description
-  );
-  const dueDate = createElement(
-    "div",
-    "due-date",
-    "Due Date: " + props.dueDate
+    props.description ? props.description : "..."
   );
   const priority = createElement(
     "div",
-    "priority",
-    "Priority: " + props.priority
+    "priority " + props.priority,
+    capitalize(props.priority) + " priority"
   );
 
-  todoCard.append(title, description, dueDate, priority);
+  // Append card elements
+  todoCard.append(title, dueDate, description, priority);
 
   return todoCard;
 }
