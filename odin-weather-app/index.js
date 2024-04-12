5d58b9d7b4cfabf09d52563f76cbbf73dc820891
Hussein Kandil <101815486+hussein-m-kandil@users.noreply.github.com>
2024-04-12 18:05:30 +0200

    Odin Weather App: Show weather data

--- odin-weather-app/index.js
+++ odin-weather-app/index.js
@@ -2,17 +2,28 @@ import './index.css';
 
 import createElement from './helpers/createElement';
 import getWeatherData from './helpers/getWeatherData';
+import showWeatherData from './helpers/showWeatherData';
 import LocationForm from './components/LocationForm';
 import Toggler from './components/Toggler';
 
+const WEATHER_CONTENT_CLASS = 'weather-content';
 const WEATHER_UNITS = ['Celsius', 'Fahrenheit'];
 let currentWeatherUnit = WEATHER_UNITS[0];
+let currentWeatherData = null;
 
 const appTitle = createElement('h1', 'app-title', 'Odin Weather App');
 
 const locationForm = LocationForm((location) =>
   getWeatherData(location)
-    .then(console.dir)
+    .then((weatherData) => {
+      console.dir(weatherData);
+      currentWeatherData = weatherData;
+      showWeatherData(
+        WEATHER_CONTENT_CLASS,
+        currentWeatherData,
+        currentWeatherUnit,
+      );
+    })
     .catch((error) =>
       console.log(`${error.name} after 'getWeatherData': \n\t${error.message}`),
     ),
@@ -20,10 +31,16 @@ const locationForm = LocationForm((location) =>
 
 const toggler = Toggler(WEATHER_UNITS, ({ value }) => {
   currentWeatherUnit = value;
-  console.log(`The current weather unit is: ${currentWeatherUnit}`);
+  if (currentWeatherData) {
+    showWeatherData(
+      WEATHER_CONTENT_CLASS,
+      currentWeatherData,
+      currentWeatherUnit,
+    );
+  }
 });
 
-const weatherContent = createElement('div', 'weather-content');
+const weatherContent = createElement('div', WEATHER_CONTENT_CLASS);
 
 const footer = createElement('footer', 'link-back');
 footer.append(
