333d56ce1b596bc096ab87fe8c216045a44d9267
Hussein Kandil <101815486+hussein-m-kandil@users.noreply.github.com>
2024-04-13 16:09:35 +0200

    Odin Weather App: Rename location to the retrieved

--- odin-weather-app/helpers/showWeatherData.js
+++ odin-weather-app/helpers/showWeatherData.js
@@ -5,6 +5,11 @@ export default function showWeatherData(
   weatherData,
   weatherUnit,
 ) {
+  const locationInput = document.querySelector('input.location');
+  if (locationInput) {
+    locationInput.value = weatherData.location.name;
+  }
+
   const contentDiv = document.querySelector(`.${containerClassName}`);
   if (contentDiv) {
     [...contentDiv.children].forEach((child) => child.remove());
