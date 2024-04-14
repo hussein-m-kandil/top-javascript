87bf3c15074eb5bdb267a14affacd2b46bb58547
Hussein Kandil <101815486+hussein-m-kandil@users.noreply.github.com>
2024-04-14 10:48:11 +0200

    Odin Weather App: Add weather/hour in WeatherInfo

--- odin-weather-app/components/WeatherInfo/index.js
+++ odin-weather-app/components/WeatherInfo/index.js
@@ -1,6 +1,7 @@
 import './index.css';
 
 import createElement from '../../helpers/createElement';
+import { format, isAfter } from 'date-fns';
 
 function createCard(title, content, className) {
   const card = createElement('div', `card ${className}`);
@@ -12,35 +13,56 @@ function createCard(title, content, className) {
 }
 
 export default function WeatherInfo(weatherData, weatherUnit) {
+  const currentWeather = weatherData.current;
+  const todayWeather = weatherData.forecast.forecastday[0].day;
+  const todayHoursWeather = weatherData.forecast.forecastday[0].hour;
+  const unit = weatherUnit.toLowerCase();
   let cTemp, feel, hTemp, lTemp;
 
-  if (weatherUnit.toLowerCase() === 'celsius') {
-    feel = weatherData.current.feelslike_c;
-    cTemp = weatherData.current.temp_c;
-    hTemp = weatherData.forecast.forecastday[0].day.maxtemp_c;
-    lTemp = weatherData.forecast.forecastday[0].day.mintemp_c;
+  if (unit === 'celsius') {
+    feel = currentWeather.feelslike_c;
+    cTemp = currentWeather.temp_c;
+    hTemp = todayWeather.maxtemp_c;
+    lTemp = todayWeather.mintemp_c;
   } else {
-    cTemp = weatherData.current.temp_f;
-    feel = weatherData.current.feelslike_f;
-    hTemp = weatherData.forecast.forecastday[0].day.maxtemp_f;
-    lTemp = weatherData.forecast.forecastday[0].day.mintemp_f;
+    cTemp = currentWeather.temp_f;
+    feel = currentWeather.feelslike_f;
+    hTemp = todayWeather.maxtemp_f;
+    lTemp = todayWeather.mintemp_f;
   }
 
   const weatherInfo = createElement('div', 'current-weather');
+
+  // Current weather
   const hero = createElement('div', 'weather-hero');
   const extras = createElement('div', 'weather-extras');
-
   hero.append(
     createElement('div', 'current-temp', `${cTemp}°`),
-    createElement('div', 'condition', `${weatherData.current.condition.text}`),
+    createElement('div', 'condition', `${currentWeather.condition.text}`),
     createElement('span', 'feels-like', ` Feels like ${feel}°`),
   );
   extras.append(
     createCard('High', `${hTemp}°`, 'high-temp'),
     createCard('Low', `${lTemp}°`, 'low-temp'),
-    createCard('Humidity', `${weatherData.current.humidity}%`, 'humidity'),
+    createCard('Humidity', `${currentWeather.humidity}%`, 'humidity'),
   );
-  weatherInfo.append(hero, extras);
+
+  // Today hours weather
+  const hoursWeather = createElement('div', 'hours-weather');
+  for (let i = 0; i < todayHoursWeather.length; i++) {
+    const hourInfo = todayHoursWeather[i];
+    if (isAfter(hourInfo.time, new Date())) {
+      hoursWeather.append(
+        createCard(
+          format(hourInfo.time, 'hh:mm a'),
+          unit === 'celsius' ? `${hourInfo.temp_c}°` : `${hourInfo.temp_f}°`,
+          'hour-temp',
+        ),
+      );
+    }
+  }
+
+  weatherInfo.append(hero, extras, hoursWeather);
 
   return weatherInfo;
 }
