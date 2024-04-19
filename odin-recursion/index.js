840f6ef78442e7a13f5388608fdcaedfc256ebb4
Hussein Kandil <101815486+hussein-m-kandil@users.noreply.github.com>
2024-04-19 13:07:05 +0200

    Odin Recursion: Add fibonacci's logic

--- odin-recursion/index.js
+++ odin-recursion/index.js
@@ -1,3 +1,21 @@
 import './index.css';
 
-console.log('Hello Odin Recursion!');
+// Fibonacci logic
+function validateFibInput(inp) {
+  const i = Number(inp);
+  return /^\d+$/.test(inp) && Number.isInteger(i) && i > 0;
+}
+function fibonacci(num) {
+  const memo = {};
+  return (function fib(n) {
+    if (memo[n] !== undefined) return memo[n];
+    memo[n] = n < 2 ? n : fib(n - 1) + fib(n - 2);
+    return memo[n];
+  })(num);
+}
+
+// TODO: Fibonacci UI
+
+// TODO: Merge Sort logic
+
+// TODO: Merge Sort UI
