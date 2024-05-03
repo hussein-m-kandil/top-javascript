fae911c63037eb13b10d3d288bce30a87bdffc5e
Hussein Kandil <101815486+hussein-m-kandil@users.noreply.github.com>
2024-05-03 17:42:28 +0300

    Odin Linked List: Document LinkedList module

--- odin-linked-list/linked-list.js
+++ odin-linked-list/linked-list.js
@@ -1,6 +1,12 @@
-import Node from './node';
+import Node from './node.js';
 
-// TODO: JSDoc
+/**
+ * @module LinkedList
+ */
+
+/**
+ * Linked list class.
+ */
 export default class LinkedList {
   #head = null;
 
@@ -20,6 +26,24 @@ export default class LinkedList {
     return this.#size;
   }
 
+  /**
+   * Constructor of LinkedList class. An instance of LinkedList has the following:
+   * - append(value): adds a new node containing value to the end of the list
+   * - prepend(value): adds a new node containing value to the start of the list
+   * - size: returns the total number of nodes in the list
+   * - head: returns the first node in the list
+   * - tail: returns the last node in the list
+   * - at(index): returns the node at the given index
+   * - pop: removes the last element from the list
+   * - contains(value): returns true if the passed in value is in the list and otherwise returns false.
+   * - find(value): returns the index of the node containing value, or null if not found.
+   * - toString: represents your LinkedList objects as strings, so you can print them out and preview them in the console.
+   *   The format should be: ( value ) -> ( value ) -> ( value ) -> null
+   * - insertAt(value, index): that inserts a new node with the provided value at the given index.
+   * - removeAt(index): that removes the node at the given index.
+   * @param  {...any} values - Any number of any values to fill the instance with.
+   * @returns {LinkedList}
+   */
   constructor(...values) {
     // If there is any given values, fill the list with them
     if (values.length > 0) {
