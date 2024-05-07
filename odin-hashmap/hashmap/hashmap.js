c90d2cfa7ca192148347f30c70038c4028b2f1e3
Hussein Kandil <101815486+hussein-m-kandil@users.noreply.github.com>
2024-05-07 18:06:59 +0300

    Odin HashMap: Add set/get & their private helpers

--- odin-hashmap/hashmap/hashmap.js
+++ odin-hashmap/hashmap/hashmap.js
@@ -1,15 +1,104 @@
-export default class HashMap {
-  #buckets = new Array(16);
+import LinkedList from '../../odin-linked-list/linked-list';
 
-  constructor() {
+export default class HashMap {
+  #buckets = Array(16);
+
+  #length = 0;
+
+  get length() {
+    return this.#length;
+  }
+
+  constructor(...args) {
+    if (args.length > 0) {
+      throw TypeError(`HashMap constructor does not expect any arguments!`);
+    }
     Object.freeze(this); // Freeze every new instance
   }
 
-  getBucket(index) {
+  static #getStringKeyAndValidate(key) {
+    if (typeof key === 'object') {
+      throw TypeError('The "key" (first argument) cannot be of TYPE "object"');
+    }
+    if (key === '') {
+      throw TypeError('The "key" (first argument) cannot be EMPTY "string"');
+    }
+    return String(key);
+  }
+
+  #getBucket(index) {
     if (index < 0 || index >= this.#buckets.length) {
       throw new Error('Trying to access index out of bound');
     }
     return this.#buckets[index];
+  }
+
+  #setBucket(index, nodeListValue) {
+    if (index < 0 || index >= this.#buckets.length) {
+      throw new Error('Trying to access index out of bound');
+    }
+    this.#buckets[index] = nodeListValue;
+  }
+
+  #hash(key) {
+    const PRIME_NUMBER = 31;
+    let hash = 0;
+    for (let i = 0; i < key.length; i++) {
+      hash += (key.charCodeAt(i) * PRIME_NUMBER) % this.#buckets.length;
+    }
+    hash %= this.#buckets.length;
+    console.log(key, hash);
+    return hash;
+  }
+
+  set(...args) {
+    if (args.length < 2) {
+      throw TypeError('The "set" method expects 2 arguments: .set(key, value)');
+    }
+    const keyValuePair = [args[0], args[1]];
+    const hash = this.#hash(HashMap.#getStringKeyAndValidate(keyValuePair[0]));
+    const bucketLinkedList = this.#getBucket(hash);
+    if (bucketLinkedList === undefined) {
+      const newLinkedList = new LinkedList();
+      newLinkedList.append(keyValuePair);
+      this.#setBucket(hash, newLinkedList);
+    } else {
+      let notFound = true;
+      let node = bucketLinkedList.head;
+      while (node !== null) {
+        const [key] = node.value;
+        if (key === keyValuePair[0]) {
+          node.value = keyValuePair;
+          notFound = false;
+          break;
+        }
+        node = node.nextNode;
+      }
+      if (notFound) {
+        bucketLinkedList.append(keyValuePair);
+      }
+    }
+    this.#length++;
+    // TODO: Scale up of the size factor has been exceeded
+    return this;
+  }
+
+  get(...args) {
+    if (args.length < 1) {
+      throw TypeError('The "get" method expects 1 argument: .get(key)');
+    }
+    const key = args[0];
+    const hash = this.#hash(HashMap.#getStringKeyAndValidate(key));
+    const bucketLinkedList = this.#getBucket(hash);
+    if (bucketLinkedList !== undefined) {
+      let node = bucketLinkedList.head;
+      while (node !== null) {
+        const keyValuePair = node.value;
+        if (keyValuePair[0] === key) return keyValuePair[1];
+        node = node.nextNode;
+      }
+    }
+    return null;
   }
 }
 
--- odin-hashmap/hashmap/hashmap.test.js
+++ odin-hashmap/hashmap/hashmap.test.js
@@ -14,6 +14,11 @@ test('should be a constructor', () => {
   expect(() => new HashMap()).not.toThrowError();
 });
 
+test('should the constructor throw Error on call with arguments', () => {
+  expect(() => new HashMap(['blah', 3], ['cool', 7])).toThrowError();
+  expect(() => new HashMap()).not.toThrowError();
+});
+
 test('should be the constructor of all instances of it', () => {
   const hashMap1 = new HashMap();
   const hashMap2 = new HashMap();
@@ -33,5 +38,86 @@ test('should class/instance member cannot be added & class prototype cannot be c
   }).toThrowError();
   expect(() => {
     Object.setPrototypeOf(HashMap.prototype, {});
+  }).toThrowError();
+});
+
+test('should "set" be a function exist on an instance and throw an error if the given number of arguments < 2', () => {
+  const hashMap = new HashMap();
+  expect(hashMap.set).toBeDefined();
+  expect(typeof hashMap.set).toBe('function');
+  expect(() => hashMap.set()).toThrowError();
+  expect(() => hashMap.set('foo')).toThrowError();
+  expect(() => hashMap.set('foo', 7)).not.toThrowError();
+  expect(() => hashMap.set('foo', 7, 'blah')).not.toThrowError();
+});
+
+test('should "get" be a function exist on an instance and throw an Error if "key" argument is not given', () => {
+  const hashMap = new HashMap();
+  expect(hashMap.get).toBeDefined();
+  expect(typeof hashMap.get).toBe('function');
+  expect(() => hashMap.get()).toThrowError();
+});
+
+test('should "set"/"get" throw an error if the key is of type "object" or the key is empty string', () => {
+  const invalidKeys = [{}, [], null, ''];
+  const validKeys = [
+    undefined,
+    false,
+    0,
+    -0,
+    +0,
+    0n,
+    0.777,
+    NaN,
+    Infinity,
+    'x',
+  ];
+  const value = 7;
+  const hashMap = new HashMap();
+  invalidKeys.forEach((key) => {
+    expect(() => hashMap.set(key, value)).toThrowError();
+    expect(() => hashMap.get(key)).toThrowError();
+  });
+  validKeys.forEach((key) => {
+    expect(() => hashMap.set(key, value)).not.toThrowError();
+    expect(() => hashMap.get(key)).not.toThrowError();
+  });
+});
+
+test('should "set" return same instance it is called on', () => {
+  const hashMap = new HashMap();
+  expect(hashMap.set('key', 'value')).toBeInstanceOf(HashMap);
+  expect(hashMap.set('key', 'value')).toBe(hashMap);
+  expect(hashMap.set('key', 'value').set('value', 'key')).toBe(hashMap);
+});
+
+test('should "get" & "length" return correct result after "setting" some keys', () => {
+  const testValues = [
+    ['foo', 7],
+    [true, false],
+    [0n, 'BigInt'],
+    ['foo', 'Again'],
+    ['oof', 'collision'],
+  ];
+  const hashMap = new HashMap();
+  testValues.forEach(([key, value]) => {
+    expect(() => hashMap.set(key, value)).not.toThrowError();
+    expect(hashMap.get(key)).toBe(value);
+  });
+  expect(hashMap.length).toBe(testValues.length);
+  expect(hashMap.get('Unknown key!')).toBe(null);
+});
+
+test('should length be immutable', () => {
+  const hashMap = new HashMap();
+  expect(hashMap.length).toBe(0);
+  expect(() => {
+    hashMap.length = 1;
+  }).toThrowError();
+  expect(() => {
+    hashMap.length++;
+  }).toThrowError();
+  expect(() => {
+    hashMap.length += 1;
   }).toThrowError();
 });
