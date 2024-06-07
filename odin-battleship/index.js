de7736a81ef497d33540e2692cb0df656aaf8fb0
Hussein Kandil <101815486+hussein-m-kandil@users.noreply.github.com>
2024-06-07 17:12:31 +0300

    Odin Battleship: Append players' info to the DOM
    Append the players' information to the DOM while creating each player
    as an object holds its UI/Logic information.

--- odin-battleship/index.js
+++ odin-battleship/index.js
@@ -1,12 +1,24 @@
 import './index.css';
 
-import { Ship } from './ship';
+import { createElement } from './helpers/createElement';
+import { PlayerInfo } from './components/player-info';
+import { Player } from './player';
 
-console.log('Hello from Odin Battleship!');
+// Create game players
+const players = [
+  { player: Player('human'), info: PlayerInfo('player-one') },
+  {
+    player: Player('computer'),
+    info: PlayerInfo('player-two'),
+  },
+];
 
-const carrier = Ship(5);
-console.log(
-  'This is a Carrier Ship: ',
-  carrier,
-  `\nLength: ${carrier.length}\nHits: ${carrier.hits}\nIs Sunk: ${carrier.isSunk()}`,
-);
+// Header
+const head = createElement('h1', 'head', 'Odin BattleShip');
+
+// Players information
+const playersInfo = createElement('div', 'players-info');
+playersInfo.append(players[0].info, players[1].info);
+
+// Append the components to the DOM
+document.body.append(head, playersInfo);
