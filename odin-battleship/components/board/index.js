140f46d1c2a6d304f17ef51ee8a0dafa6540ca7d
Hussein Kandil <101815486+hussein-m-kandil@users.noreply.github.com>
2024-06-09 17:31:54 +0300

    Odin Battleship: Create disabled/hidden boards

--- odin-battleship/components/board/index.js
+++ odin-battleship/components/board/index.js
@@ -1,9 +1,38 @@
 import './index.css';
 
 import { createElement } from '../../helpers/createElement';
+import { GameBoard } from '../../game-board';
 
-export default function Board(playerGameBoard) {
-  const board = createElement('div', 'board-container');
+/**
+ * Creates a player's board UI component
+ * @param {GameBoard} playerGameBoard - An instance of 'GameBoard'
+ * @param {boolean} hidden - Hidden player's ships indicator
+ * @param {boolean} disabled - Disabled player's board indicator (to ignore click events)
+ * @returns {HTMLDivElement}
+ */
+export default function Board(playerGameBoard, hidden, disabled) {
+  [playerGameBoard, hidden, disabled].forEach((arg) => {
+    if (arg === undefined) {
+      throw TypeError("'Board' is called with an invalid number of arguments!");
+    }
+  });
+
+  if (!(playerGameBoard instanceof GameBoard)) {
+    throw TypeError(
+      `'Board' is called with invalid 'playerGameBoard'! Given '${playerGameBoard}'.`,
+    );
+  }
+
+  if (typeof hidden !== 'boolean' || typeof disabled !== 'boolean') {
+    throw TypeError(
+      `'Board' expects 'hidden' & 'disabled' of type 'boolean'! Given '${hidden} & ${disabled}'.`,
+    );
+  }
+
+  const board = createElement(
+    'div',
+    'board-container' + (disabled ? ' disabled' : ''),
+  );
 
   let boardWidth = 0;
 
@@ -11,13 +40,16 @@ export default function Board(playerGameBoard) {
     boardWidth = row.length;
     row.forEach((cell, j) => {
       let className = 'board-cell';
-      className += cell.ship ? ' ship' : '';
+      className += cell.ship && !hidden ? ' ship' : '';
       className += cell.attacked ? ' attacked' : '';
       className += cell.missed ? ' missed' : '';
       const boardCell = createElement('div', className);
-      boardCell.addEventListener('click', () => {
-        playerGameBoard.receiveAttack([i, j]);
-      });
+      if (!disabled) {
+        boardCell.addEventListener('click', () => {
+          playerGameBoard.receiveAttack([i, j]);
+          console.log('ATTACK!'); // TODO: TO BE DELETED
+        });
+      }
       board.appendChild(boardCell);
     });
   });
