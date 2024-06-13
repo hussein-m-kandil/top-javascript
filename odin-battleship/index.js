ad7de51506e1ccaee1643b24c04f4fb43318ce16
Hussein Kandil <101815486+hussein-m-kandil@users.noreply.github.com>
2024-06-13 18:53:04 +0300

    Odin Battleship: Refactor the game controller

--- odin-battleship/index.js
+++ odin-battleship/index.js
@@ -1,76 +1,10 @@
 import './index.css';
 
-import { gameEvents } from './game-events';
 import { createElement } from './helpers/create-element';
-import { PlayerInfo } from './components/player-info';
-import { Board } from './components/board';
-import { Player } from './player';
+import { createGame } from './helpers/create-game';
+import { gameEvents } from './game-events';
 
-// Create game object to hold the state of the game
-const game = {
-  playersData: [
-    {
-      name: 'first-player',
-      player: Player(Player.TYPES.HUMAN),
-      playerUI: createElement('div', 'first-player'),
-    },
-    {
-      name: 'second-player',
-      player: Player(Player.TYPES.COMPUTER),
-      playerUI: createElement('div', 'second-player'),
-    },
-  ],
-  allPlayersDisabled: false,
-  currentPlayerIndex: 0,
-  /**
-   * Switches the current player's index
-   */
-  switchCurrentPlayer() {
-    this.currentPlayerIndex = (this.currentPlayerIndex + 1) % 2;
-  },
-  /**
-   * Renders the recent player's UI-information/board to the DOM
-   * @param {number} playerIndex
-   * @param {HTMLDivElement?} parentElement - Optional parent to append player's UI to it
-   */
-  renderPlayerUI(playerIndex, parentElement) {
-    const playerData = this.playersData[playerIndex];
-    const currentPlayer = playerIndex === this.currentPlayerIndex;
-    const disabled = this.allPlayersDisabled || currentPlayer;
-    const playerTypeComputer = playerData.player.type === Player.TYPES.COMPUTER;
-    const clickable = playerTypeComputer && !currentPlayer && !disabled;
-    [...playerData.playerUI.children].forEach((child) => child.remove());
-    playerData.playerUI.append(
-      PlayerInfo(playerData.name, playerData.player.type),
-      Board(
-        playerData.player.gameBoard,
-        playerData.player.type === Player.TYPES.COMPUTER,
-        disabled,
-        clickable,
-      ),
-    );
-    if (parentElement) parentElement.append(playerData.playerUI);
-  },
-  /**
-   * Renders the recent ALL player's UIs-information/board to the DOM
-   * @param {HTMLDivElement} parentElement - Optional parent to append ALL player's UIs to it
-   */
-  renderAllPlayersUI(parentElement) {
-    for (let i = 0; i < game.playersData.length; i++) {
-      game.renderPlayerUI(i, parentElement);
-    }
-  },
-};
-
-// Play on a computer turn
-const playIfComputerTurn = () => {
-  const currentPlayer = game.playersData[game.currentPlayerIndex].player;
-  if (currentPlayer.type === Player.TYPES.COMPUTER) {
-    setTimeout(() => {
-      currentPlayer.play();
-    }, 1000);
-  }
-};
+const game = createGame();
 
 // Create a flag to be 'true' while evaluating an attack
 let evaluatingAttack = false;
@@ -89,23 +23,27 @@ gameEvents.add(gameEvents.ATTACK, (cellPlacePair) => {
 gameEvents.add(gameEvents.HIT, () => {
   // Continue with the current player, but rerender the opponent's board
   game.renderPlayerUI(game.currentPlayerIndex === 0 ? 1 : 0);
-  playIfComputerTurn();
+  game.playIfComputerTurn();
 });
 
 gameEvents.add(gameEvents.MISS, () => {
   game.switchCurrentPlayer();
   game.renderAllPlayersUI();
-  playIfComputerTurn();
+  game.playIfComputerTurn();
 });
 
 gameEvents.add(gameEvents.LOSS, () => {
+  gameEvents.emit(gameEvents.GAME_OVER);
+});
+
+gameEvents.add(gameEvents.GAME_OVER, () => {
   game.allPlayersDisabled = true;
   game.renderAllPlayersUI();
   // TODO: Present a game over message & give the option to play again
 });
 
 // Header
-const head = createElement('h1', 'head', 'Odin BattleShip');
+const head = createElement('h1', 'head', 'Odin Battleship');
 
 // Game container
 const gameContainer = createElement('div', 'game-container');
@@ -113,10 +51,5 @@ const gameContainer = createElement('div', 'game-container');
 // Initiate player's UI
 game.renderAllPlayersUI(gameContainer);
 
-// TODO: Add the ability to change who plays first
-
 // Append the components to the DOM
 document.body.append(head, gameContainer);
-
-// Start the game
-playIfComputerTurn();
