258ff810fc8b64ffbd858373442e58c9fef76093
Hussein Kandil <101815486+hussein-m-kandil@users.noreply.github.com>
2024-06-14 16:35:03 +0300

    Odin Battleship: Handle GAME_OVER & show modal

--- odin-battleship/index.js
+++ odin-battleship/index.js
@@ -2,17 +2,37 @@ import './index.css';
 
 import { createElement } from './helpers/create-element';
 import { createGame } from './helpers/create-game';
+import { gameEvents } from './game-events';
 
-// Header
-const head = createElement('h1', 'head', 'Odin Battleship');
+const [header, gameContainer] = [
+  createElement('h1', 'head', 'Odin Battleship'),
+  createElement('div', 'game-container'),
+];
 
-// Game container
-const gameContainer = createElement('div', 'game-container');
-
-// Append the components to the DOM
-document.body.append(head, gameContainer);
-
-// Initiate the logic of the game
+// Initiate a game logic object
 const game = createGame(gameContainer);
 
+gameEvents.add(gameEvents.GAME_OVER, () => {
+  game.endCurrentGame();
+  const endMessage = game.isComputerWon() ? 'Game Over!' : 'You Win!';
+  const [dialog, message, question, cancelBtn, playAgainBtn] = [
+    createElement('dialog', 'game-over'),
+    createElement('h2', 'message', endMessage),
+    createElement('p', 'question', 'Do want to play again?'),
+    createElement('button', 'cancel-btn', 'Cancel', ['type', 'button']),
+    createElement('button', 'play-again-btn', 'Play Again', ['type', 'button']),
+  ];
+  dialog.addEventListener('close', () => document.body.removeChild(dialog));
+  cancelBtn.addEventListener('click', () => dialog.close());
+  playAgainBtn.addEventListener('click', () => {
+    game.startNewGame();
+    dialog.close();
+  });
+  dialog.append(message, question, cancelBtn, playAgainBtn);
+  document.body.append(dialog);
+  dialog.showModal();
+});
+
 game.startNewGame();
+
+document.body.append(header, gameContainer);
