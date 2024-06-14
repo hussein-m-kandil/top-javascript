3dca1e0f68aeb6d38800266f7285e8031faaa814
Hussein Kandil <101815486+hussein-m-kandil@users.noreply.github.com>
2024-06-14 16:25:15 +0300

    Odin Battleship: Add most game logic to createGame

--- odin-battleship/helpers/create-game.js
+++ odin-battleship/helpers/create-game.js
@@ -1,13 +1,14 @@
 import { createElement } from './create-element';
+import { gameEvents } from '../game-events';
 import { PlayerInfo } from '../components/player-info';
 import { Board } from '../components/board';
 import { Player } from '../player';
 
 /**
  * Creates a game object to hold the state of the game
- * @returns @type {BattleshipGame}
+ * @returns @type {Object}
  */
-export default function createGame() {
+function createNewState() {
   return {
     playersData: [
       {
@@ -21,8 +22,8 @@ export default function createGame() {
         playerUI: createElement('div', 'second-player'),
       },
     ],
-    allPlayersDisabled: false,
     currentPlayerIndex: 0,
+    allPlayersDisabled: false,
     /**
      * Switches the current player's index
      */
@@ -81,6 +82,86 @@ export default function createGame() {
         this.playersData[this.currentPlayerIndex].player.type ===
         Player.TYPES.COMPUTER
       );
+    },
+  };
+}
+
+let gameState = createNewState();
+
+let evaluatingAttack = false;
+
+/** Handles ATTACK event */
+function attackHandler(cellPlacePair) {
+  if (!evaluatingAttack) {
+    evaluatingAttack = true;
+    const opponentIndex = gameState.currentPlayerIndex === 0 ? 1 : 0;
+    const opponentPlayer = gameState.playersData[opponentIndex].player;
+    opponentPlayer.gameBoard.receiveAttack(cellPlacePair);
+    evaluatingAttack = false;
+  }
+}
+
+/** Handles HIT event */
+function hitHandler() {
+  // Continue with the current player, but rerender the opponent's board
+  gameState.renderPlayerUI(gameState.currentPlayerIndex === 0 ? 1 : 0);
+  gameState.playIfComputerTurn();
+}
+
+/** Handles MISS event */
+function missHandler() {
+  gameState.switchCurrentPlayer();
+  gameState.renderAllPlayersUI();
+  gameState.playIfComputerTurn();
+}
+
+/** Handles LOSS event */
+function lossHandler() {
+  gameEvents.emit(gameEvents.GAME_OVER);
+}
+
+/** Adds all game events' handlers */
+function addGameEventsHandlers() {
+  gameEvents.add(gameEvents.ATTACK, attackHandler);
+  gameEvents.add(gameEvents.HIT, hitHandler);
+  gameEvents.add(gameEvents.MISS, missHandler);
+  gameEvents.add(gameEvents.LOSS, lossHandler);
+}
+
+/** Removes all game events' handlers */
+function removeGameEventsHandlers() {
+  gameEvents.remove(gameEvents.ATTACK, attackHandler);
+  gameEvents.remove(gameEvents.HIT, hitHandler);
+  gameEvents.remove(gameEvents.MISS, missHandler);
+  gameEvents.remove(gameEvents.LOSS, lossHandler);
+}
+
+/**
+ * Creates a game object to hold the state of the game
+ * @returns @type {BattleshipGame}
+ */
+export default function createGame(gameContainer) {
+  return {
+    gameContainer,
+    /**
+     * - Resets game's state/events.
+     * - Rerenders the game's UI after removing the old UI from the game's container.
+     */
+    startNewGame() {
+      removeGameEventsHandlers();
+      gameState = createNewState();
+      [...this.gameContainer.children].forEach((child) => child.remove());
+      gameState.renderAllPlayersUI(this.gameContainer);
+      addGameEventsHandlers();
+    },
+    /** Checks whether the current player is the computer (should be called on a player's loss) */
+    isComputerWon() {
+      return gameState.isComputerCurrentPlayer();
+    },
+    /** Renders disabled UI */
+    endCurrentGame() {
+      gameState.allPlayersDisabled = true;
+      gameState.renderAllPlayersUI();
     },
   };
 }
--- odin-battleship/index.js
+++ odin-battleship/index.js
@@ -2,45 +2,6 @@ import './index.css';
 
 import { createElement } from './helpers/create-element';
 import { createGame } from './helpers/create-game';
-import { gameEvents } from './game-events';
-
-const game = createGame();
-
-// Create a flag to be 'true' while evaluating an attack
-let evaluatingAttack = false;
-
-// Handle game events
-gameEvents.add(gameEvents.ATTACK, (cellPlacePair) => {
-  if (!evaluatingAttack) {
-    evaluatingAttack = true;
-    const opponentIndex = game.currentPlayerIndex === 0 ? 1 : 0;
-    const opponentPlayer = game.playersData[opponentIndex].player;
-    opponentPlayer.gameBoard.receiveAttack(cellPlacePair);
-    evaluatingAttack = false;
-  }
-});
-
-gameEvents.add(gameEvents.HIT, () => {
-  // Continue with the current player, but rerender the opponent's board
-  game.renderPlayerUI(game.currentPlayerIndex === 0 ? 1 : 0);
-  game.playIfComputerTurn();
-});
-
-gameEvents.add(gameEvents.MISS, () => {
-  game.switchCurrentPlayer();
-  game.renderAllPlayersUI();
-  game.playIfComputerTurn();
-});
-
-gameEvents.add(gameEvents.LOSS, () => {
-  gameEvents.emit(gameEvents.GAME_OVER);
-});
-
-gameEvents.add(gameEvents.GAME_OVER, () => {
-  game.allPlayersDisabled = true;
-  game.renderAllPlayersUI();
-  // TODO: Present a game over message & give the option to play again
-});
 
 // Header
 const head = createElement('h1', 'head', 'Odin Battleship');
@@ -48,8 +9,10 @@ const head = createElement('h1', 'head', 'Odin Battleship');
 // Game container
 const gameContainer = createElement('div', 'game-container');
 
-// Initiate player's UI
-game.renderAllPlayersUI(gameContainer);
-
 // Append the components to the DOM
 document.body.append(head, gameContainer);
+
+// Initiate the logic of the game
+const game = createGame(gameContainer);
+
+game.startNewGame();
