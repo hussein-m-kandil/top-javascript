35cf6aefe0f03283f34593ce61c34ce79cf03cf8
Hussein Kandil <101815486+hussein-m-kandil@users.noreply.github.com>
2024-06-18 21:16:17 +0300

    Odin Battleship: Add move-ship input in PlayerInfo

--- odin-battleship/components/player-info/index.js
+++ odin-battleship/components/player-info/index.js
@@ -7,36 +7,88 @@ import { Player } from '../../player';
 /**
  * Creates DOM element (div) that has the player's information
  * @param {string} name - The name of the player (hyphened not spaced)
+ * @param {string} type - The type of the player
+ * @param {Object} gameBoard - The player's game board
  * @returns {HTMLDivElement}
  */
-export default function PlayerInfo(name, type) {
+export default function PlayerInfo(name, type, gameBoard) {
   if (typeof name !== 'string') {
     throw TypeError(
       `Invalid 'name' type; expected: 'string', given: '${name}'`,
     );
   }
 
-  let typeIndicator;
-
-  if (type === Player.TYPES.COMPUTER) {
-    typeIndicator = 'COMPUTER';
-  } else if (type === Player.TYPES.HUMAN) {
-    typeIndicator = 'YOU';
-  } else {
-    throw TypeError(`Invalid player type!`);
-  }
-
   const playerInfo = createElement('div', `${name}-info`);
 
   const nameIndicator = capitalize(name.replace('-', ' '));
 
-  const playerName = createElement(
-    'h3',
-    'player-name',
-    `${nameIndicator} (${typeIndicator})`,
-  );
+  const playerName = createElement('h3', 'player-name', `${nameIndicator}`);
 
   playerInfo.append(playerName);
+
+  if (gameBoard && type === Player.TYPES.HUMAN) {
+    const shipNav = createElement('div', 'ship-nav');
+    const selectShip = createElement('select', 'select-ship', null, [
+      'name',
+      'select-ship',
+    ]);
+    gameBoard.shipsAreas.forEach((shipArea, i) => {
+      selectShip.appendChild(
+        createElement(
+          'option',
+          'ship-option',
+          ''.padStart(shipArea.length, 'o'),
+          ['value', i],
+        ),
+      );
+    });
+    const [moveUpBtn, moveDownBtn, moveLeftBtn, moveRightBtn] = [
+      createElement('button', 'move-up-btn', 'Up', ['type', 'button']),
+      createElement('button', 'move-down-btn', 'Down', ['type', 'button']),
+      createElement('button', 'move-left-btn', 'Left', ['type', 'button']),
+      createElement('button', 'move-right-btn', 'Right', ['type', 'button']),
+    ];
+    moveUpBtn.addEventListener('click', () => {
+      if (selectShip.value) {
+        const shipAreaIndex = Number(selectShip.value);
+        if (Number.isInteger(shipAreaIndex)) {
+          gameBoard.moveShipUp(shipAreaIndex);
+        }
+      }
+    });
+    moveDownBtn.addEventListener('click', () => {
+      if (selectShip.value) {
+        const shipAreaIndex = Number(selectShip.value);
+        if (Number.isInteger(shipAreaIndex)) {
+          gameBoard.moveShipDown(shipAreaIndex);
+        }
+      }
+    });
+    moveLeftBtn.addEventListener('click', () => {
+      if (selectShip.value) {
+        const shipAreaIndex = Number(selectShip.value);
+        if (Number.isInteger(shipAreaIndex)) {
+          gameBoard.moveShipLeft(shipAreaIndex);
+        }
+      }
+    });
+    moveRightBtn.addEventListener('click', () => {
+      if (selectShip.value) {
+        const shipAreaIndex = Number(selectShip.value);
+        if (Number.isInteger(shipAreaIndex)) {
+          gameBoard.moveShipRight(shipAreaIndex);
+        }
+      }
+    });
+    shipNav.append(
+      selectShip,
+      moveUpBtn,
+      moveDownBtn,
+      moveLeftBtn,
+      moveRightBtn,
+    );
+    playerInfo.appendChild(shipNav);
+  }
 
   return playerInfo;
 }
